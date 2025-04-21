0ac963edc3567291b025a67c9cea2e90755760fc
Andy Loughran <andy@lockran.com>
2025-04-21 11:20:35 +0100

    Changed data path

--- chapter_02/start/js/main.js
+++ chapter_02/start/js/main.js
@@ -3,7 +3,7 @@ const svg = d3.select(".responsive-svg-container")
         .attr("viewBox","0 0 600 700")
         .style("border", "1px solid black")
 
-d3.csv("../start/data/data.csv", d => {
+d3.csv("./data/data.csv", d => {
     return {
         technology: d.technology,
         count: +d.count
