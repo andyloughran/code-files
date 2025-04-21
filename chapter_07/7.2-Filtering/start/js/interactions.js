b2f5fabe4d9a4c4b52b12c17e0779b9266ac94cf
Andy Loughran <andy@lockran.com>
2025-04-21 11:20:14 +0100

    First half chapter 7

--- chapter_07/7.2-Filtering/start/js/interactions.js
+++ chapter_07/7.2-Filtering/start/js/interactions.js
@@ -4,6 +4,23 @@
 const populateFilters = (data) => {
 
   // Create the filters here
+  d3.select("#filters")
+    .selectAll(".filter")
+    .data(filters)
+    .join("button")
+      .attr("class", d=> `filter ${d.isActive ? "active" : ""}`)
+      .text(d => d.label)
+      .on("click", (e, d) => {
+        if (!d.isActive) {
+          filters.forEach(filter => {
+            filter.isActive = d.id === filter.id ? true : false;
+          });
+          d3.selectAll(".filter")
+            .classed("active", filter => filter.id === d.id ? true : false);
+          
+          updateHistogram(d.id, data);
+        }
+      })
 
 };
 
@@ -11,8 +28,17 @@ const populateFilters = (data) => {
 /****************************/
 /*   Update the histogram   */
 /****************************/
-const updateHistogram = (selectedFilter, data) => {
+const updateHistogram = (filterId, data) => {
   
   // Update the histogram here
+  const updatedData = filterId === "all" ? data : data.filter(respondent => respondent.gender === filterId);
+  const updatedBins = binGenerator(updatedData);
 
+  d3.selectAll("#histogram rect")
+    .data(updatedBins)
+    .transition()
+      .duration(1500)
+      .ease(d3.easeCubicOut)
+      .attr("y", d=> yScale(d.length))
+      .attr("height", d=> innerHeight - yScale(d.length));
 };
